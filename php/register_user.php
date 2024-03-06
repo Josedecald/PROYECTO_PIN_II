88f3291d4935ee4c31196f3ebc05af7083af4940
Jose Calderon <josedecald@gmail.com>
2024-03-06 18:44:33 -0500

    prueba db

--- php/register_user.php
+++ php/register_user.php
@@ -1,12 +1,12 @@
 <?php
-    include 'conexion_BD.php';
+    include('conexion_BD.php');
 
         $Nombre = $_POST['Nombre'];
         $Apellido = $_POST['Apellido'];
         $Email = $_POST['Email'];
-        $Contrasena = $_POST['Contraseña'];
+        $Contrasena = $_POST['Contrasena'];
 
-        $query = "INSERT INTO USUARIOS (NOMBRE, APELLIDO, CORREO, CONTRASEÑA)
+        $query = "INSERT INTO registro_usuarios (NOMBRE, APELLIDO, CORREO, CONTRASEÑA)
                 VALUES ('$Nombre', '$Apellido', '$Email', '$Contrasena')";
 
         $result = mysqli_query($conexion, $query);
