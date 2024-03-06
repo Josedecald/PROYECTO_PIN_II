ceb1ace018d265d5f82fd1b00b51b9630250e69a
Jose Calderon <josedecald@gmail.com>
2024-03-05 21:34:56 -0500

    nada

--- php/conexion_BD.php
+++ php/conexion_BD.php
@@ -1,3 +1,13 @@
 <?php
+$host = "localhost:3306";
+$database = "usuarios";
+$user = "root";
+$password = "123456";
 
-    $conexion = mysqli_connect("bokzfymrzwfpccbqsjza-mysql.services.clever-cloud.com", "uxqieajd9dm1llbe", "QrGkyfXIT8cd4hf4ik7v", "bokzfymrzwfpccbqsjza");
+$conexion = mysqli_connect($host, $user, $password, $database);
+
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+} else {
+    echo "Conexión exitosa a la base de datos";
+}
