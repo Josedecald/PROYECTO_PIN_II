10cc8460661dc73a5539bce2739215b7d2f5aecf
Jose Calderon <josedecald@gmail.com>
2024-03-12 08:07:58 -0500

    Actualizacion calendario

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('Nav');
+    if (window.scrollY > 50) {
+        navbar.classList.remove('transparent');
+        navbar.classList.add('white-bg');
+    } else {
+        navbar.classList.remove('white-bg');
+        navbar.classList.add('transparent');
+    }
+});
